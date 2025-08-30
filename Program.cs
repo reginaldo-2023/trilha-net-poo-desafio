e4b26f4fa8f85a9f30a38a7b4d7711a6526cf37a
reginaldo <reginaldo-a@hotmail.com>
2025-08-29 22:03:47 -0300

    Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("119999999", "Nokia 3310", "123456789", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Iphone iphone = new Iphone("118888888", "iPhone 14", "987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
